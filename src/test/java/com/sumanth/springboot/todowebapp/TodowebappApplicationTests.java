43cfeb8975290cdb415fa8464f0b07429c2055c5
SumanthReddy1228 <sumanth0129@gmai.com>
2025-04-26 20:18:24 +0530

    first commit

---
+++ src/test/java/com/sumanth/springboot/todowebapp/TodowebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumanth.springboot.todowebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodowebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
